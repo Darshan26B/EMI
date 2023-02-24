0c70b86bd749fa13d97c84e2e2c39badccc0bd0c
Darshan26B <iamdarshan6239db@gmail.com>
2023-02-24 19:57:51 +0530

    commit2

--- app/src/main/java/com/ca/emicalculator/MainActivity.java
+++ app/src/main/java/com/ca/emicalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.ca.emicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,32 +12,40 @@ import android.widget.TextView;
 import java.time.Month;
 
 public class MainActivity extends AppCompatActivity {
-    int c, i;
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editTextLoanAmount = findViewById(R.id.loan);
-        EditText editTextInterestRate = findViewById(R.id.rate);
-        EditText editTextMonths = findViewById(R.id.months);
-        Button button = findViewById(R.id.Ans);
-        TextView textView = findViewById(R.id.FinalAns);
-        button.setOnClickListener(new View.OnClickListener() {
+        EditText Amount = findViewById(R.id.loan);
+        EditText Interest = findViewById(R.id.rate);
+        EditText Months = findViewById(R.id.months);
+        Button cal = findViewById(R.id.Ans);
+        TextView Emi = findViewById(R.id.Emi);
+        TextView TotalAmount = findViewById(R.id.TotalAmount);
+        TextView TotalInterest = findViewById(R.id.TotalInterest);
+
+
+        cal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                float LoanAmount = Float.parseFloat(String.valueOf(editTextLoanAmount.getText()));
-                float InterestRate = Float.parseFloat(String.valueOf(editTextInterestRate.getText()));
-                float Months = Float.parseFloat(String.valueOf(editTextMonths.getText()));
-               for (i=0; i<R.id.months; i++) {
-                   c=c*(1+R.id.rate);
-               }
-               float EMI=R.id.loan*R.id.rate*(c/(c-1));
-                textView.setText(String.valueOf("EMI="+EMI));
+                int Amt = Integer.parseInt(Amount.getText().toString());
+                double interest = Double.parseDouble(Interest.getText().toString());
+                int months = Integer.parseInt(Months.getText().toString());
+
+                double r = interest / (12 * 100);
+                double cal = 1;
+                for (int i = 0; i < months; i++) {
+                    cal *= (1 + r);
+                }
+                double ans = Amt * r * (cal / (cal - 1));
+                Emi.setText("EMI = " + ans);
+                double total = ans * months;
+                TotalAmount.setText("Total Amount =" + total);
+                double irate = total - Amt;
+                TotalInterest.setText("Interest Amount = " + irate);
 
             }
-
         });
-
     }
 }
